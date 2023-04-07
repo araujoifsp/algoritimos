a0e166ee58776468944b1b9424cd3df8a4e5ae2d
gcmonteirodev <gcm020701@gmail.com>
2023-04-07 13:26:19 -0300

    timer

--- desempenho.c
+++ desempenho.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "timer.h"
+#include <sys/time.h>
 
+
+void timer(void (*funcao)(),long *res, int i) {
+    struct timeval inicio, fim;
+    gettimeofday(&inicio, NULL);
+
+    funcao();
+
+    gettimeofday(&fim, NULL);
+    long s = fim.tv_sec - inicio.tv_sec;
+    res[i] = s;
+}
+
+void media(long *res, int tamanho){
+    long soma = 0;
+    for(int i = 0; i < tamanho; i++){
+        soma += res[i];
+    }
+    double media = (double)soma / tamanho;
+    printf("Tempo médio de execução: %.5f segundos.\n", media);
+}
--- desempenho.h
+++ desempenho.h
@@ -1,1 +1,6 @@
+#include <stdio.h>
+#include <sys/time.h>
 
+typedef struct timeval;
+void timer(void (*funcao)(),long *res, int i);
+void media(long*res, int tamanho);
